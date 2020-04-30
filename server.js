ae37452159f554cdd9d90e918f55223f6fc206b6
Usman Akhtar <akhtar_usman@outlook.com>
2020-04-29 22:41:45 -0400

    implemented authentication using Auth0

--- server.js
+++ server.js
@@ -2,10 +2,40 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const router = express.Router();
+const { auth } = require("express-openid-connect");
+require("dotenv").config();
+const { requiresAuth } = require("express-openid-connect");
+
+const config = {
+  required: false,
+  auth0Logout: true,
+  appSession: {
+    secret: process.env.AUTH_SECRET
+  },
+  baseURL: "http://localhost:3000",
+  clientID: process.env.AUTH_CLIENTID,
+  issuerBaseURL: process.env.AUTH_ISSUER
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+  res.sendDile(
+    req.isAuthenticated()
+      ? path.join(__dirname + "/../frontend/dashboard.html")
+      : path.join(__dirname + "/../frontend/index.html")
+  );
+});
 
 app.use(express.static(path.join(__dirname, "/../frontend")));
 
-router.get("/", function(req, res) {
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.openid.user));
+});
+
+router.get("/callback", function(req, res) {
   res.sendFile(path.join(__dirname + "/../frontend/index.html"));
 });
 
