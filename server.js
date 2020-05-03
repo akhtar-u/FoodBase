2c788f3a4d22ab05922c518f74189795af32e357
Usman Akhtar <akhtar_usman@outlook.com>
2020-05-03 00:55:54 -0400

    fixed some auth issues, removed useless routes

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
-const bodyParser = require("body-parser");
-const { auth } = require("express-openid-connect");
-const { requiresAuth } = require("express-openid-connect");
-require("dotenv").config();
-const recipe = require("./routes/recipe.route");
-const mongoose = require("mongoose");
+const path = require('path');
+const bodyParser = require('body-parser');
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
+require('dotenv').config();
+const recipe = require('./routes/recipe.route');
+const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
 });
 const db = mongoose.connection;
-db.once("open", () => {
-  console.log("MongoDB database connected");
+db.once('open', () => {
+  console.log('MongoDB database connected');
 });
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const config = {
   required: false,
@@ -24,7 +24,7 @@ const config = {
   appSession: {
     secret: process.env.AUTH_SECRET,
   },
-  baseURL: "http://localhost:3000",
+  baseURL: 'http://localhost:3000',
   clientID: process.env.AUTH_CLIENTID,
   issuerBaseURL: process.env.AUTH_ISSUER,
 };
@@ -36,23 +36,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 // serve static files
-app.use(express.static(path.join(__dirname, "/../frontend")));
+app.use(express.static(path.join(__dirname, 'public')));
 // use recipe routes
-app.use("/", recipe);
+app.use('/recipe', recipe);
 
-// main login route
-app.get("/", (req, res) => {
-  res.sendFile(
-    req.isAuthenticated()
-      ? path.join(__dirname + "/../frontend/dashboard.html")
-      : path.join(__dirname + "/../frontend/index.html")
-  );
-});
-
-// profile info route
-app.get("/profile", requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.openid.user));
+app.get('/dashboard', requiresAuth(), (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/dashboard.html'));
+  let userid = req.openid.user.sub;
 });
 
 app.listen(process.env.port || 3000);
-console.log("Running at Port 3000");
+console.log('Running at Port 3000');
