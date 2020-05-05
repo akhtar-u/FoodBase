7c44f1ab99b40de27180937b1dfc8517efc7bb43
Usman Akhtar <akhtar_usman@outlook.com>
2020-05-04 20:58:02 -0400

    added different pages and added ajax requests to db

--- client.js
+++ client.js
@@ -1,3 +1,24 @@
-function signIn() {
-  window.location = "dashboard.html";
+$.ajax({
+  url: 'http://localhost:3000/profile',
+  method: 'GET',
+  success: function (data) {
+    getRecipes(data.sub);
+    document.getElementById('userdb').innerHTML = data.nickname + "'s Recipes";
+  },
+});
+
+// query recipes using userid
+function getRecipes(userid) {
+  $.ajax({
+    url: 'http://localhost:3000/recipe/' + '/' + userid,
+    method: 'GET',
+  })
+    .then(function (data) {
+      let htmlstr = data;
+      console.log(htmlstr[1]);
+      $('#rec-1').html(JSON.stringify(htmlstr[0]));
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 }
