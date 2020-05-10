3775375e3d00473b097ed0f42701aae94661c142
Usman Akhtar <akhtar_usman@outlook.com>
2020-05-10 00:39:54 -0400

    added search by user

--- controllers/recipe.controller.js
+++ controllers/recipe.controller.js
@@ -27,7 +27,10 @@ exports.recipes_list = (req, res) => {
 
 // fetch all recipes that are public
 exports.recipe_public = (req, res) => {
-  Recipe.find({ public: req.params.id }, 'image recipename public -_id')
+  Recipe.find(
+    { public: req.params.id },
+    'image recipename nickname public -_id'
+  )
     .then((recipe) => res.json(recipe))
     .catch((err) => res.status(400).json('Error: ' + err));
 };
